621b055d32fb6ddb5092bd8507ab0ff041756390
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 12:32:13 +0530

    Add: API call to fetch data

--- app/src/main/java/com/example/catfacts/MainActivity.kt
+++ app/src/main/java/com/example/catfacts/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.catfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.catfacts.api.ApiClient
+import com.example.catfacts.models.CatFacts
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val call = ApiClient.apiService.getAllCatFacts()
+
+        call.enqueue(object : Callback<ArrayList<CatFacts>> {
+            override fun onResponse(
+                call: Call<ArrayList<CatFacts>>,
+                response: Response<ArrayList<CatFacts>>
+            ) {
+                if(response.isSuccessful) {
+                    val catFacts = response.body()
+                } else {
+                    Log.i("Oops","OOps")
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<CatFacts>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
