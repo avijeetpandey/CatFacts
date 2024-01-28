bf7065eeb71fb87e23073d308e229db8f809e986
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 13:56:28 +0530

    Add: UI for the facts

--- app/src/main/java/com/example/catfacts/MainActivity.kt
+++ app/src/main/java/com/example/catfacts/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.catfacts
 
+import CatFactsAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.catfacts.api.ApiClient
 import com.example.catfacts.models.CatFacts
+import com.example.catfacts.viewModel.CatFactsViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private var recyclerView: RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+
+        fetchCatFactsData()
     }
 
     // function to fetch the facts from the internet
@@ -26,6 +37,14 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if(response.isSuccessful) {
                     val catFacts = response.body()
+                    val dataSet = ArrayList<CatFactsViewModel>()
+                    catFacts?.forEach {
+                        it?.text?.let {
+                            dataSet.add(CatFactsViewModel(it))
+                        }
+                    }
+                    val adapter = CatFactsAdapter(dataSet = dataSet)
+                    recyclerView?.adapter = adapter
                 } else {
                     Log.i("Oops","OOps")
                 }
--- app/src/main/java/com/example/catfacts/adapter/CatFactsAdapter.kt
+++ app/src/main/java/com/example/catfacts/adapter/CatFactsAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.catfacts.R
 import com.example.catfacts.viewModel.CatFactsViewModel
 
-class CatFactsAdapter(private val dataSet: Array<CatFactsViewModel>) :
+class CatFactsAdapter(private val dataSet: ArrayList<CatFactsViewModel>) :
     RecyclerView.Adapter<CatFactsAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
